d9910847e53ccfa333b91076c89f5392c9c68b64
cauchzira <dfishchenko@yandex.ru>
2021-10-18 21:47:28 +0700

    add some styles and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
+import MessageField from "./components/MessageField";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MessageField />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Message({message}) {
+  return (
+    <div className="message__wrapp">
+      <p className="message__wrapp__text_message">{message}</p>
+    </div>
+  );
+}
+
+export default Message;
