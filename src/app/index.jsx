9436dbd8e888a0abbac71c6d2f5d3ce8146174b1
cauchzira <dfishchenko@yandex.ru>
2021-10-28 22:15:20 +0700

    добавил отступы

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -34,6 +34,13 @@ const theme = createMuiTheme({
           padding: "10px 16px"
         }
       }
+    },
+    MuiListItem: {
+      styleOverrides: {
+        root: {
+          margin: "16px 0"
+        }
+      }
     }
   }
 });
